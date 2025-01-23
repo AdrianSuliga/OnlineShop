564e402c6ad5cfadd9b02d983cf1930a88fa42fc
DominikMat <dominikmat@student.agh.edu.pl>
2025-01-23 14:51:19 +0100

    sortowanie po cenie

--- frontend/components/ProductList.tsx
+++ frontend/components/ProductList.tsx
@@ -17,6 +17,7 @@ const ProductList: React.FC  = () => {
   const [loading, setLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('all');
+  const [selectedOrdering, setOrdering] = useState('Suggested');
   const [filteredProducts, setFilteredProducts] = useState<ProductType[]>(products);
   const [categories, setCategories] = useState<string[]>([]);
 
@@ -50,9 +51,13 @@ const ProductList: React.FC  = () => {
     if (selectedCategory !== 'all') {
       filtered = filtered.filter(product => product.category === selectedCategory);
     }
+    switch(selectedOrdering) {
+      case "Price Asc": filtered = filtered.sort((a,b) => -a.price + b.price); break;
+      case "Price Desc": filtered = filtered.sort((a,b) => a.price - b.price); break;
+    } 
 
     setFilteredProducts(filtered);
-  }, [searchQuery, selectedCategory, products]);
+  }, [searchQuery, selectedCategory, products, selectedOrdering]);
 
   useEffect(() => {
     const uniqueCategories = ['all', ...new Set(products.map(product => product.category))];
@@ -88,7 +93,7 @@ const ProductList: React.FC  = () => {
                   onChange={(e) => setSelectedCategory(e.valueOf())}
                   style={{  
                     height:'auto',
-                    marginRight:'20px',
+                    marginRight:'5px',
                     border: 'black 1px'
                   }}
                 >
@@ -97,6 +102,24 @@ const ProductList: React.FC  = () => {
                       {category === 'all' ? 'All Categories' : category}
                       </option>
                   ))}
+              </Select>
+
+              {/* Dropdown sortowania */}
+              <Select
+                  value={selectedOrdering}
+                  onChange={(e) => setOrdering(e.valueOf())}
+                  style={{  
+                    height:'auto',
+                    width:'auto',
+                    marginRight:'20px',
+                    border: 'black 1px'
+                  }}
+                >
+                <Select.Option value="Suggested">Suggested</Select.Option>
+                <Select.Option value="Price Asc">Price Asc</Select.Option>
+                <Select.Option value="Price Desc">Price Desc</Select.Option>
+                <Select.Option value="Rating Asc">Rating Asc</Select.Option>
+                <Select.Option value="Rating Desc">Rating Desc</Select.Option>
               </Select>
             </div>
 
